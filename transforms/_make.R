6bdd92daa61ab574cb4ba06a80101e7fd55d8b21
craig-shenton <craig.shenton@nhs.net>
2022-12-16 22:34:28 +0000

    targets formatting

--- transforms/_make.R
+++
@@ -1,6 +0,0 @@
-
-library(targets)
-
-tar_config_set(script = "transforms/_targets.R")
-
-tar_make()
--- transforms/_targets.R
+++ transforms/_targets.R
@@ -6,33 +6,44 @@
 # -------------------------------------------------------------------------
 
 # FILE:           _targets.R
-# DESCRIPTION:    Ingestion functions
+# DESCRIPTION:    Define target functions
 # CONTRIBUTORS:   Craig R. Shenton
 # CONTACT:        craig.shenton@nhs.net
 # CREATED:        16 Dec 2022
 # VERSION:        0.0.1
 
 library(targets)
+tar_config_set(script = "transforms/_targets.R")
+tar_option_set(packages = c("readr", "here", "NHSRdatasets"))
 
 source("utilities/create_directory.R")
 source("utilities/get_nhsr_dataset.R")
 source("utilities/sink_csv.R")
-tar_option_set(packages = c("readr", "here", "NHSRdatasets"))
 
+# Define 'load_ae_attendance_data' target
+# -------------------------------------------------------------------------
 nhsr_dataset_name <- "ae_attendances"
+load_ae_attendance_data <- tar_target(
+  name = input,
+  command = get_nhsr_dataset(nhsr_dataset_name)
+)
+
+# Define 'sink_csv_to_raw' target
+# -------------------------------------------------------------------------
 data_path <- "data"
 end_state <- "raw"
 source <- "NHSRdatasets"
 sink_file <- "ae_attendances.csv"
-
-list(
-  tar_target(name = input,
-             command = get_nhsr_dataset(nhsr_dataset_name)),
-  tar_target(name = output,
-             command = sink_csv(input,
-                                     data_path,
-                                     end_state,
-                                     source,
-                                     sink_file),
-                                     format = "file")
+sink_csv_to_raw <- tar_target(
+  name = output,
+  command = sink_csv(input,
+                     data_path,
+                     end_state,
+                     source,
+                     sink_file),
+  format = "file"
 )
+
+# Set target order
+# -------------------------------------------------------------------------
+list(load_ae_attendance_data, sink_csv_to_raw)
